d36730b01146c6823c1b335c92d263e4f0942a22
viniciusfullsale <vinicius3010@gmail.com>
2026-03-02 11:29:09 -0300

    add instanciação store no main

--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -18,29 +18,24 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { computed } from 'vue';
+
+import { useKanbanStore } from '@/stores/store'
+
+const kanbanStore = useKanbanStore();
 
 import KabanColumn from './KabanColumn.vue';
 
-const tasks_todo = ref([
-  { id: 1, title: 'task a fazer 1'},
-  { id: 2, title: 'task a fazer 2'},
-  { id: 3, title: 'task a fazer 3'},
-  { id: 4, title: 'task a fazer 4'},
-]);
+const tasks_todo = computed(() =>{
+  kanbanStore.tasks.filter(t=> t.state === 'todo');
+});
 
-const tasks_doing= ref([
-  { id: 5, title: 'task fazendo 1'},
-  { id: 6, title: 'task fazendo 2'},
-  { id: 7, title: 'task fazendo 3'},
-  { id: 8, title: 'task fazendo 4'},
-]);
+const tasks_doing = computed(() =>{
+  kanbanStore.tasks.filter(t=> t.state === 'doing');
+});
 
-const tasks_done= ref([
-  { id: 9, title: 'task feita 1'},
-  { id: 10, title: 'task feita 2'},
-  { id: 11, title: 'task feita 3'},
-  { id: 12, title: 'task feita 4'},
-]);
+const tasks_done = computed(() =>{
+  kanbanStore.tasks.filter(t=> t.state === 'done');
+});
 
 </script>
